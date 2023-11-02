6e14d763ede481eb030b8200772d2367f01e4679
gusti9506 <gusti9506@gmail.com>
2023-11-02 09:36:14 -0300

    Cambios al 02/11/23

--- scripts.js
+++ scripts.js
@@ -1,32 +1,73 @@
-console.log("Hola Mundo");
-let cantidad = document.getElementById('cantidad');
-let select = document.getElementById('categoria');
 
+let nombre = document.getElementById('nombre');
+let apellido = document.getElementById('apellido');
+let correo = document.getElementById('correo');
+let cantidadTickets = document.getElementById('cantidad');
+let seleccionarCategoria = document.getElementById('categoria');
 
-cantidad.addEventListener("blur", function(e) {
-    let cuenta = e.target.value;
-    console.log("Total sin Descuento: ", cuenta * 200);
-    let compra = cuenta * 200;
+nombre.addEventListener("blur", function(e){
+    let campoNombre = e.target;
+    let campoVacio = e.target.value;
+    if (campoVacio.length == 0){
+        campoNombre.classList.add('sombraError');
+        campoNombre.nextElementSibling.classList.add('error');
+        campoNombre.nextElementSibling.innerText = "Ingrese Nombre";
+    }else {
+        campoNombre.classList.remove('sombraError');
+        campoNombre.nextElementSibling.classList.remove('error');
+        campoNombre.nextElementSibling.innerText = "";
+    }
+});
+apellido.addEventListener("blur", function(e){
+    let campoApellido = e.target;
+    let apellidoVacio = e.target.value;
+    if (apellidoVacio === ""){
+        campoApellido.classList.add('sombraError');
+        campoApellido.nextElementSibling.classList.add('error');
+        campoApellido.nextElementSibling.innerText = "Ingrese Apellido";
+    }else {
+        campoApellido.classList.remove('sombraError');
+        campoApellido.nextElementSibling.classList.remove('error');
+        campoApellido.nextElementSibling.innerText = "";
+    }
+});
+correo.addEventListener("blur", function(e){
+    let campoCorreo = e.target;
+    let correoVacio = e.target.value;
+    if (correoVacio.length === 0){
+        campoCorreo.classList.add('sombraError');
+        campoCorreo.nextElementSibling.classList.add('error');
+        campoCorreo.nextElementSibling.innerText = "Ingrese Email";
+    } else {
+        campoCorreo.classList.remove('sombraError');
+        campoCorreo.nextElementSibling.classList.remove('error');
+        campoCorreo.nextElementSibling.innerText = "";
+    }
+});
 
-
-    select.addEventListener("click", function (x) {
-        let selectedOption = x.target.value;
-        console.log("Categoria seleccionada: " + selectedOption);
-        if (selectedOption == "Estudiante") {
-            let total = document.getElementById('total').value = descuento(compra, 20);
-            document.getElementById('total').value = "Total a pagar: $ " + total;
-            console.log("Total a Pagar(80% Desc): ", total);
+cantidadTickets.addEventListener("blur", function(e) {
+    e.preventDefault();
+    let totalTickets = e.target.value;
+    let compraTickets = totalTickets * 200;
+    console.log("Total sin Descuento: ", compraTickets);
+    seleccionarCategoria.addEventListener("click", function (x) {
+        let opcionSeleccinada = x.target.value;
+        console.log("Categoria seleccionada: " + opcionSeleccinada);
+        if (opcionSeleccinada == "Estudiante") {
+            let totalPagar = document.getElementById('total').value = descuento(compraTickets, 20);
+            document.getElementById('total').value = "Total a pagar: $ " + totalPagar;
+            console.log("Total a Pagar(80% Desc): ", totalPagar);
         }
 
-        if (selectedOption == "Trainee") {
-            let total = document.getElementById('total').value = descuento(compra, 50);
-            document.getElementById('total').value = "Total a pagar: $ " + total;
-            console.log("Total a Pagar (50% Desc): ", total);
+        if (opcionSeleccinada == "Trainee") {
+            let totalPagar = document.getElementById('total').value = descuento(compraTickets, 50);
+            document.getElementById('total').value = "Total a pagar: $ " + totalPagar;
+            console.log("Total a Pagar (50% Desc): ", totalPagar);
         }
 
-        if (selectedOption == "Junior") {
-            let total = document.getElementById('total').value = descuento(compra, 85);
-            document.getElementById('total').value = "Total a pagar: $ " + total;
+        if (opcionSeleccinada == "Junior") {
+            let totalPagar = document.getElementById('total').value = descuento(compraTickets, 85);
+            document.getElementById('total').value = "Total a pagar: $ " + totalPagar;
             console.log("Total a Pagar(15% Desc): ", document.getElementById('total').value);
         }
         btnBorrar();
@@ -45,13 +86,14 @@ function btnBorrar() {
        let formulario = document.getElementById('formulario');
        formulario.addEventListener("click", function(){
             formulario.onreset(); 
-       })
+       });
     });
 }
 
 function btnResumen(){
     let btnResumen = document.getElementById('btn-resumen');
     btnResumen.addEventListener("click", function() {
+
         let aPagar = document.getElementById('total').value;
         let nombre = document.getElementById('nombre').value;
         let apellido = document.getElementById('apellido').value;
